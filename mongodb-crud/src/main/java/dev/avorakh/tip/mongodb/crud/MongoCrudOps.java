5288de74fa2293350304db34c567922a73ad0769
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2025-05-29 23:22:46 +0200

    TIP-91: add create ops

---
+++ mongodb-crud/src/main/java/dev/avorakh/tip/mongodb/crud/MongoCrudOps.java
@@ -0,0 +1,98 @@
+package dev.avorakh.tip.mongodb.crud;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.InsertOneResult;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+public class MongoCrudOps {
+    private static final Logger LOGGER = Logger.getLogger(MongoCrudOps.class.getName());
+    private static final String URI_PROPERTY_KEY = "mongodb.uri";
+
+    private static final String BANK_DB_NAME = "bank";
+    private static final String COLLECTION_NAME = "accounts";
+
+    private final MongoCollection<Document> collection;
+
+    public MongoCrudOps(MongoClient client, String dbName, String collectionName) {
+        MongoDatabase database = client.getDatabase(dbName);
+        this.collection = database.getCollection(collectionName);
+    }
+
+    public ObjectId crete(Document document) {
+        InsertOneResult result = collection.insertOne(document);
+        return result.getInsertedId().asObjectId().getValue();
+    }
+
+    public List<ObjectId> create(List<Document> documents) {
+        var result = collection.insertMany(documents);
+        var insertedIds = new ArrayList<ObjectId>();
+        result.getInsertedIds()
+                .forEach((key, value) -> insertedIds.add(value.asObjectId().getValue()));
+        return insertedIds;
+    }
+
+    public static void main(String[] args) {
+        try (var client = MongoClients.create(
+                Optional.ofNullable(System.getProperty(URI_PROPERTY_KEY)).orElseThrow())) {
+
+            // Insert a Document
+            var sampleTrainingCrudOps = new MongoCrudOps(client, "sample_training", "inspections");
+
+            var inspection = new Document("_id", new ObjectId())
+                    .append("id", "10021-2015-ENFO")
+                    .append("certificate_number", 9278806)
+                    .append("business_name", "ATLIXCO DELI GROCERY INC.")
+                    .append(
+                            "date",
+                            Date.from(LocalDate.of(2025, 2, 20)
+                                    .atStartOfDay(ZoneId.systemDefault())
+                                    .toInstant()))
+                    .append("result", "No Violation Issued")
+                    .append("sector", "Cigarette Retail Dealer - 127")
+                    .append(
+                            "address",
+                            new Document()
+                                    .append("city", "RIDGEWOOD")
+                                    .append("zip", 11385)
+                                    .append("street", "MENAHAN ST")
+                                    .append("number", 1712));
+            var inspectionId = sampleTrainingCrudOps.crete(inspection);
+
+            LOGGER.info("✅ Inserted inspection: " + inspectionId);
+
+            // Insert Multiple Documents
+            var bankAccountsCrudOps = new MongoCrudOps(client, BANK_DB_NAME, COLLECTION_NAME);
+
+            var doc1 = new Document()
+                    .append("account_holder", "john doe")
+                    .append("account_id", "MDB99115881")
+                    .append("balance", 1785)
+                    .append("account_type", "checking");
+
+            var doc2 = new Document()
+                    .append("account_holder", "jane doe")
+                    .append("account_id", "MDB79101843")
+                    .append("balance", 1468)
+                    .append("account_type", "checking");
+
+            var newAccounts = List.of(doc1, doc2);
+            var createdAccountIds = bankAccountsCrudOps.create(newAccounts);
+            LOGGER.info("✅ Inserted accounts: " + createdAccountIds);
+
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "❌ MongoDB operation failed", e);
+        }
+    }
+}
